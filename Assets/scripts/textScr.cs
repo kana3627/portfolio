e5e8d3dbcae2a6ea6b6ee473850c41b3cc9c3d8c
Nakano <mangadaisuki4S@gmail.com>
2019-12-05 13:34:13 +0900

    ハイスコアを表示させた

--- Assets/scripts/textScr.cs
+++ Assets/scripts/textScr.cs
@@ -6,20 +6,19 @@ using UnityEngine.UI;
 //UIを管理するスクリプト
 public class textScr : MonoBehaviour
 {
-    public Text score;
-    public Text Ready;
-    public Text gameclear;
+    public Text score;      //スコア
+    public Text Ready;      //最初に出すreadyの文字
+    public Text gameclear;  //アイテムを全部取り切ったら表示するテキスト
+    public Text highScoreText;  //ハイスコアを表示する
 
-    public Image zanki0;
-    public Image zanki1;
-    public Image zanki2;
-    public Image zanki3;
+    public int scoreNum;    //スコアを表示する変数
+    public int scoreCount;  //集めたアイテムの数
 
-    public int scoreNum;
-    public int scoreCount;
+    private static int highScore;
+    private string key = "HIGH SCORE";
 
-    GameObject[] itemObject;
-    int itemNum;
+    GameObject[] itemObject; //シーン上にあるアイテムを保持する配列
+    int itemNum;             //配列の要素数からシーン上にあるアイテムの数を求める
 
     void Start()
     {
@@ -29,20 +28,32 @@ public class textScr : MonoBehaviour
         itemObject = GameObject.FindGameObjectsWithTag("item");
         itemNum = itemObject.Length;
 
+        highScore = PlayerPrefs.GetInt(key, 0);
+        highScoreText.text = highScore.ToString();
+
     }
 
 
     void Update()
     {
-        score.text = "SCORE:" + scoreNum;  //itemCountで渡された
+        score.text = "SCORE:" + scoreNum;  //itemCountで渡された値と敵を食べた時の値を表示
         Invoke("readyDisplay", 3.0f);
 
+        //集めたアイテム数が、シーン上にあるアイテムの数を超えたらgameclearの文字を表示
         if (scoreCount >= itemNum)
         {
             gameclear.enabled = true;
+            if (scoreNum > highScore)
+            {
+                highScore = scoreNum;
+                PlayerPrefs.SetInt(key, highScore);
+                highScoreText.text = "HISCORE" + highScore;
+            }
         }
+
     }
 
+    //readyの文字を非表示にする関数
     void readyDisplay()
     {
 
